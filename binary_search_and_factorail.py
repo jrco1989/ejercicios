5ef4a274d3a64b3233fced28661b8b0d4741403d
jrco1989 <jrco1989@hotamil.com>
2019-12-15 20:59:42 -0500

    agregando factorial

--- binary_search_and_factorail.py
+++ binary_search_and_factorail.py
@@ -13,7 +13,14 @@ def binary_search(data, target, low, high):
 	else:
 		return binary_search(data, target, mid +1, high )
 
-if __name__=='__main__': #de esta manera se genera el lpunto de ingreso 
+def factorial (number):
+	if number==0 or number ==1:
+		return 1
+	elif number>1:
+		return number*factorial(number-1)
+
+
+if __name__=='__main__': #de esta manera se genera el punto de ingreso 
 	data=[]
 	data=[random.randint(0,100) for i in range(100)]
 	data.sort()
@@ -21,4 +28,10 @@ if __name__=='__main__': #de esta manera se genera el lpunto de ingreso
 	target=int( input( 'what number would you like to find?'))
 	found=binary_search(data, target, 0, len(data)-1)
 	print (found)
+
+
+if __name__=="__main__":
+	number=int (input('Enter the number to calculate your factorial'))
+	result=factorial(number)
+	print('the factorial of {} is {}'.format(number,result))
 
